3c6eb88033d95f37b77174b9459e56387a9fe1de
Thoshi-o <takao-hoshino@hands-lab.com>
2021-03-20 08:40:05 +0900

    add

--- amazon_comprehend_api/main.py
+++ amazon_comprehend_api/main.py
@@ -5,7 +5,7 @@ import pprint
 
 def main():
     print("call main")
-    common.comprehender("テスト").detect_language()
+    common.comprehender("again").detect_language()
 
 if __name__ == "__main__":
     main()
